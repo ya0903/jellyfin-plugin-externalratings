59dab16ab9b780f5a0a83072a7055a059cee60f8
ya0903 <136716539+ya0903@users.noreply.github.com>
2026-03-09 23:59:45 +0000

    Update PluginServiceRegistrator.cs

--- PluginServiceRegistrator.cs
+++ PluginServiceRegistrator.cs
@@ -1,23 +1,50 @@
+using System.Net.Http;
 using Jellyfin.Plugin.ExternalRatings.Providers;
 using Jellyfin.Plugin.ExternalRatings.Services;
-using MediaBrowser.Common.Plugins;
-using Microsoft.Extensions.DependencyInjection;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Controller.Library;
+using Microsoft.Extensions.Logging;
 
 namespace Jellyfin.Plugin.ExternalRatings;
 
 /// <summary>
-/// Registers plugin services with the Jellyfin DI container.
+/// Service locator for plugin dependencies.
+/// Instantiated by the Plugin entry point.
 /// </summary>
-public class PluginServiceRegistrator : IPluginServiceRegistrator
+public static class PluginServiceLocator
 {
-    /// <inheritdoc />
-    public void RegisterServices(IServiceCollection serviceCollection)
-    {
-        serviceCollection.AddHttpClient("ExternalRatings");
+    public static RatingCacheService? CacheService { get; private set; }
+    public static ExternalRatingService? RatingService { get; private set; }
 
-        serviceCollection.AddSingleton<RatingCacheService>();
-        serviceCollection.AddSingleton<LetterboxdRatingProvider>();
-        serviceCollection.AddSingleton<ImdbRatingProvider>();
-        serviceCollection.AddSingleton<ExternalRatingService>();
+    public static void Initialize(
+        IApplicationPaths appPaths,
+        ILibraryManager libraryManager,
+        ILoggerFactory loggerFactory)
+    {
+        var httpClientFactory = new DefaultHttpClientFactory();
+
+        CacheService = new RatingCacheService(
+            appPaths,
+            loggerFactory.CreateLogger<RatingCacheService>());
+
+        var letterboxd = new LetterboxdRatingProvider(
+            httpClientFactory,
+            loggerFactory.CreateLogger<LetterboxdRatingProvider>());
+
+        var imdb = new ImdbRatingProvider(
+            httpClientFactory,
+            loggerFactory.CreateLogger<ImdbRatingProvider>());
+
+        RatingService = new ExternalRatingService(
+            letterboxd,
+            imdb,
+            CacheService,
+            libraryManager,
+            loggerFactory.CreateLogger<ExternalRatingService>());
+    }
+
+    private sealed class DefaultHttpClientFactory : IHttpClientFactory
+    {
+        public HttpClient CreateClient(string name) => new HttpClient();
     }
 }
