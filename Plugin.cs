380a401ce04cdf550b9963bb8cc2f57e4e5c632d
ya0903 <136716539+ya0903@users.noreply.github.com>
2026-03-10 00:00:02 +0000

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -2,50 +2,38 @@ using System;
 using System.Collections.Generic;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
+using MediaBrowser.Controller.Library;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 using Microsoft.Extensions.Logging;
 
 namespace Jellyfin.Plugin.ExternalRatings;
 
-/// <summary>
-/// Jellyfin External Ratings Plugin - Letterboxd for Movies, IMDB for TV Shows.
-/// </summary>
 public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="Plugin"/> class.
-    /// </summary>
-    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+    public Plugin(
+        IApplicationPaths applicationPaths,
+        IXmlSerializer xmlSerializer,
+        ILibraryManager libraryManager,
+        ILoggerFactory loggerFactory)
         : base(applicationPaths, xmlSerializer)
     {
         Instance = this;
+        PluginServiceLocator.Initialize(applicationPaths, libraryManager, loggerFactory);
     }
 
-    /// <inheritdoc />
     public override string Name => "External Ratings";
-
-    /// <inheritdoc />
     public override Guid Id => Guid.Parse("a1b2c3d4-e5f6-7890-abcd-ef1234567890");
-
-    /// <inheritdoc />
     public override string Description => "Displays Letterboxd ratings for movies and IMDB ratings for TV shows.";
 
-    /// <summary>
-    /// Gets the current plugin instance.
-    /// </summary>
     public static Plugin? Instance { get; private set; }
 
-    /// <inheritdoc />
-    public IEnumerable<PluginPageInfo> GetPages()
+    public IEnumerable<PluginPageInfo> GetPages() => new[]
     {
-        return new[]
+        new PluginPageInfo
         {
-            new PluginPageInfo
-            {
-                Name = Name,
-                EmbeddedResourcePath = $"{GetType().Namespace}.Configuration.configPage.html"
-            }
-        };
-    }
+            Name = Name,
+            EmbeddedResourcePath = $"{GetType().Namespace}.Configuration.configPage.html"
+        }
+    };
 }
